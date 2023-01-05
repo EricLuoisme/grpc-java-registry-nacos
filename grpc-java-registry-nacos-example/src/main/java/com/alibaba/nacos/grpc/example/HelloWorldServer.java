43ee084f12dd5c414e374065615349f12da80d7a
roylic <roylic@wokoworks.com>
2023-01-05 14:55:10 +0800

    updated

--- grpc-java-registry-nacos-example/src/main/java/com/alibaba/nacos/grpc/example/HelloWorldServer.java
+++ grpc-java-registry-nacos-example/src/main/java/com/alibaba/nacos/grpc/example/HelloWorldServer.java
@@ -27,15 +27,12 @@ public class HelloWorldServer {
         properties = NacosUtils.buildNacosProperties(uri, properties);
         server.init(port, properties, bindableServices);
         server.start();
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-                                                 @Override
-                                                 public void run() {
-                                                     // Use stderr here since the logger may have been reset by its JVM shutdown hook.
-                                                     System.err.println("*** shutting down gRPC server since JVM is shutting down");
-                                                     server.stop();
-                                                     System.err.println("*** server shut down");
-                                                 }
-                                             });
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+            System.err.println("*** shutting down gRPC server since JVM is shutting down");
+            server.stop();
+            System.err.println("*** server shut down");
+        }));
 
     }
 
